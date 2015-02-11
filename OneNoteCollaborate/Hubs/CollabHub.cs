5c25c78d114371034a06c75a6c7b8323e50c6386
longhorns747 <eshernan3@gatech.edu>
2015-02-10 23:29:36 -0500

    Added synchronizing textarea

---
+++ OneNoteCollaborate/Hubs/CollabHub.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace OneNoteCollaborate.Hubs
+{
+    public class CollabHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+
+        public void syncTextbox(string currentText)
+        {
+            Clients.All.syncFromServer(currentText);
+        }
+    }
+}
--- OneNoteCollaborate/Startup.cs
+++ OneNoteCollaborate/Startup.cs
@@ -8,6 +8,7 @@ namespace OneNoteCollaborate
     {
         public void Configuration(IAppBuilder app)
         {
+            app.MapSignalR();
             ConfigureAuth(app);
         }
     }
